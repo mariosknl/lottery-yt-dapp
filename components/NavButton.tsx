cc15e0529cacecf12ab1c8b19d3732d54a82df52
mariosknl <mariosknl@outlook.com>
2022-09-11 12:34:00 +0300

    created: implementing the login/logout functionality

--- components/NavButton.tsx
+++ components/NavButton.tsx
@@ -3,11 +3,13 @@ import React from "react";
 type Props = {
 	title: string;
 	isActive?: boolean;
+	onClick?: () => void;
 };
 
-export const NavButton = ({ title, isActive }: Props) => {
+export const NavButton = ({ title, isActive, onClick }: Props) => {
 	return (
 		<button
+			onClick={onClick}
 			className={`${
 				isActive && "bg-[#036756]"
 			} hover:bg-[#036756] text-white py-2 px-4 rounded font-bold`}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,38 @@
+import { useAddress, useContract, useMetamask } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { Header } from "../components/Header";
+import { Login } from "../components/Login";
+import PropagateLoader from "react-spinners/PropagateLoader";
 
 const Home: NextPage = () => {
+	const address = useAddress();
+	const { contract, isLoading } = useContract(
+		process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS
+	);
+
+	if (isLoading) {
+		return (
+			<div className="bg-[#091B18] h-screen flex flex-col items-center justify-center">
+				<div className="flex items-center space-x-2 mb-10">
+					<img
+						className="rounded-full h-20 w-20"
+						src="https://i.imgur.com/4h7mAu7.png"
+						alt=""
+					/>
+					<h1 className="text-lg text-white font-bold">
+						Loading the PAPAFAM DRAW
+					</h1>
+				</div>
+				<PropagateLoader color="white" size={30} />
+			</div>
+		);
+	}
+
+	if (!address) {
+		return <Login />;
+	}
+
 	return (
 		<div className="bg-[#091B18] min-h-screen flex flex-col">
 			<Head>
