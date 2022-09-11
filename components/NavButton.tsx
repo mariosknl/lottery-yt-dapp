58e9baec9c4204a9764e1d9c7b482928b5617430
mariosknl <mariosknl@outlook.com>
2022-09-11 11:31:27 +0300

    created: Header & NavButton components

---
+++ components/NavButton.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+type Props = {
+	title: string;
+	isActive?: boolean;
+};
+
+export const NavButton = ({ title, isActive }: Props) => {
+	return (
+		<button
+			className={`${
+				isActive && "bg-[#036756]"
+			} hover:bg-[#036756] text-white py-2 px-4 rounded font-bold`}
+		>
+			{title}
+		</button>
+	);
+};
