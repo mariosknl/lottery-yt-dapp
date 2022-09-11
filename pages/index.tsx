5e81a3bca5dcdc4b87898e2fcfd06acbb34192f7
mariosknl <mariosknl@outlook.com>
2022-09-11 13:58:44 +0300

    createdL building the countdownTimer component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,42 @@
-import { useAddress, useContract, useMetamask } from "@thirdweb-dev/react";
+import {
+	useAddress,
+	useContract,
+	useContractData,
+	useMetamask,
+} from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { Header } from "../components/Header";
 import { Login } from "../components/Login";
 import Loading from "../components/Loading";
 import { useState } from "react";
+import { ethers } from "ethers";
+import { currency } from "../constants";
+import { CountdownTimer } from "../components/CountdownTimer";
 
 const Home: NextPage = () => {
 	const address = useAddress();
 	const [quantity, setQuantity] = useState<number>(1);
 	const { contract, isLoading } = useContract(
 		process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS
+	);
+
+	const { data: expiration } = useContractData(contract, "expiration");
+
+	const { data: remainingTickets } = useContractData(
+		contract,
+		"RemainingTickets"
+	);
+
+	const { data: currentWinningReward } = useContractData(
+		contract,
+		"CurrentWinningReward"
+	);
+
+	const { data: ticketPrice } = useContractData(contract, "ticketPrice");
+	const { data: ticketCommission } = useContractData(
+		contract,
+		"ticketCommission"
 	);
 
 	if (isLoading) return <Loading />;
@@ -22,77 +48,105 @@ const Home: NextPage = () => {
 				<title>PAPAFAM Draw</title>
 			</Head>
 
-			<Header />
+			<div className="flex-1">
+				<Header />
 
-			{/* The Next Draw box */}
-			<div className="space-y-5 md:space-y-0 m-5 md:flex md:flex-row items-start justify-center md:space-x-5">
-				<div className="stats-container">
-					<h1 className="text-5xl text-white font-semibold text-center">
-						The Next Draw
-					</h1>
-					<div className="flex justify-between p-2 space-x-2">
-						<div className="stats">
-							<h2 className="text-sm">Total Pool</h2>
-							<p className="text-xl">0.1 MATIC</p>
-						</div>
-						<div className="stats">
-							<h2 className="text-sm">Tickets Remaining</h2>
-							<p className="text-xl">100</p>
-						</div>
-					</div>
-
-					{/* Countdown Timer */}
-					{/* .... */}
-				</div>
-
-				<div className="stats-container space-y-2">
+				{/* The Next Draw box */}
+				<div className="space-y-5 md:space-y-0 m-5 md:flex md:flex-row items-start justify-center md:space-x-5">
 					<div className="stats-container">
-						<div className="flex justify-between items-center text-white pb-2">
-							<h2>Price per Ticket</h2>
-							<p>0.01 MATIC</p>
-						</div>
-
-						<div className="flex text-white items-center space-x-2 bg-[#091B18] border border-[#004377] p-4">
-							<p>TICKETS</p>
-							<input
-								type="number"
-								className="flex w-full bg-transparent text-right outline-none"
-								min={1}
-								max={10}
-								value={quantity}
-								onChange={(e) => setQuantity(Number(e.target.value))}
-							/>
-						</div>
-
-						<div className="space-y-2 mt-5">
-							<div className="flex items-center justify-between text-emerald-300 text-sm italic font-extrabold">
-								<p>Total cost of tickets</p>
-								<p>0.999</p>
+						<h1 className="text-5xl text-white font-semibold text-center">
+							The Next Draw
+						</h1>
+						<div className="flex justify-between p-2 space-x-2">
+							<div className="stats">
+								<h2 className="text-sm">Total Pool</h2>
+								<p className="text-xl">
+									{currentWinningReward &&
+										ethers.utils.formatEther(
+											currentWinningReward.toString()
+										)}{" "}
+									{currency}
+								</p>
 							</div>
-
-							<div className="flex items-center justify-between text-emerald-300 text-xs italic">
-								<p>Service fees</p>
-								<p>0.001 MATIC</p>
-							</div>
-
-							<div className="flex items-center justify-between text-emerald-300 text-xs italic">
-								<p>+ Network fees</p>
-								<p>TBC</p>
+							<div className="stats">
+								<h2 className="text-sm">Tickets Remaining</h2>
+								<p className="text-xl">{remainingTickets?.toNumber()}</p>
 							</div>
 						</div>
 
-						<button
-							disabled
-							className="mt-5 w-full bg-gradient-to-br from-orange-500 to-emerald-600 px-10 py-5 rounded-md text-white shadow-xl disabled:from-gray-600 disabled:to-gray-600 disabled:text-gray-100 disabled:cursor-not-allowed"
-						>
-							But tickets
-						</button>
+						{/* Countdown Timer */}
+						<div className="mt-5 mb-3">
+							<CountdownTimer />
+						</div>
+					</div>
+
+					<div className="stats-container space-y-2">
+						<div className="stats-container">
+							<div className="flex justify-between items-center text-white pb-2">
+								<h2>Price per Ticket</h2>
+								<p>
+									{ticketPrice &&
+										ethers.utils.formatEther(ticketPrice?.toString())}{" "}
+									{currency}
+								</p>
+							</div>
+
+							<div className="flex text-white items-center space-x-2 bg-[#091B18] border border-[#004377] p-4">
+								<p>TICKETS</p>
+								<input
+									type="number"
+									className="flex w-full bg-transparent text-right outline-none"
+									min={1}
+									max={10}
+									value={quantity}
+									onChange={(e) => setQuantity(Number(e.target.value))}
+								/>
+							</div>
+
+							<div className="space-y-2 mt-5">
+								<div className="flex items-center justify-between text-emerald-300 text-sm italic font-extrabold">
+									<p>Total cost of tickets</p>
+									<p>
+										{ticketPrice &&
+											Number(ethers.utils.formatEther(ticketPrice.toString())) *
+												quantity}{" "}
+										{currency}
+									</p>
+								</div>
+
+								<div className="flex items-center justify-between text-emerald-300 text-xs italic">
+									<p>Service fees</p>
+									<p>
+										{ticketCommission &&
+											ethers.utils.formatEther(
+												ticketCommission?.toString()
+											)}{" "}
+										{currency}
+									</p>
+								</div>
+
+								<div className="flex items-center justify-between text-emerald-300 text-xs italic">
+									<p>+ Network fees</p>
+									<p>TBC</p>
+								</div>
+							</div>
+
+							<button
+								disabled={
+									expiration?.toString() < Date.now().toString() ||
+									remainingTickets?.toNumber() === 0
+								}
+								className="mt-5 w-full bg-gradient-to-br from-orange-500 to-emerald-600 px-10 py-5 rounded-md text-white shadow-xl disabled:from-gray-600 disabled:to-gray-600 disabled:text-gray-100 disabled:cursor-not-allowed"
+							>
+								But tickets
+							</button>
+						</div>
 					</div>
 				</div>
-			</div>
 
-			{/* The price per ticket box */}
-			<div></div>
+				{/* The price per ticket box */}
+				<div></div>
+			</div>
 		</div>
 	);
 };
