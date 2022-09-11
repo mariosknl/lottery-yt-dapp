cb1eaf3e1f280398e4cffdf8f284139f9451ca55
mariosknl <mariosknl@outlook.com>
2022-09-11 12:39:30 +0300

    edit: refactor into components

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { Header } from "../components/Header";
 import { Login } from "../components/Login";
-import PropagateLoader from "react-spinners/PropagateLoader";
+import Loading from "../components/Loading";
 
 const Home: NextPage = () => {
 	const address = useAddress();
@@ -11,27 +11,8 @@ const Home: NextPage = () => {
 		process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS
 	);
 
-	if (isLoading) {
-		return (
-			<div className="bg-[#091B18] h-screen flex flex-col items-center justify-center">
-				<div className="flex items-center space-x-2 mb-10">
-					<img
-						className="rounded-full h-20 w-20"
-						src="https://i.imgur.com/4h7mAu7.png"
-						alt=""
-					/>
-					<h1 className="text-lg text-white font-bold">
-						Loading the PAPAFAM DRAW
-					</h1>
-				</div>
-				<PropagateLoader color="white" size={30} />
-			</div>
-		);
-	}
-
-	if (!address) {
-		return <Login />;
-	}
+	if (isLoading) return <Loading />;
+	if (!address) return <Login />;
 
 	return (
 		<div className="bg-[#091B18] min-h-screen flex flex-col">
